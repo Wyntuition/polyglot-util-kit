43464c6c9da08112613d945d4ab59aa365f74a4e
Wyn Van Devanter <wyn.vandevanter@excella.com>
2018-01-10 11:43:05 -0500

    tweak pg dump

--- postgres/postgres-backup.sh
+++ postgres/postgres-backup.sh
@@ -1,16 +1,31 @@
+#!/usr/bin/env bash
+#
+# Get dump of database
+# USAGE:
+#   postgres-backup.sh <HOSTNAME> <PG_USERNAME> <DATABASE_NAME> <SCHEMA_NAME>
+#
+
 DATE=`date +%Y-%m-%d`
 FILENAME=postgres-backup-$DATE.sql
-HOST=<ENTER>
+HOST=localhost
 if [ $1 ]; then 
   HOST=$1
 fi 
-DATABASE_NAME=postgres
+PG_USERNAME=postgres
 if [ $2 ]; then 
-  DATABASE_NAME=$2
-fi 
+  PG_USERNAME=$2
+fi
+DATABASE_NAME=postgres
+if [ $3 ]; then 
+  DATABASE_NAME=$3
+fi
+SCHEMA_NAME=local_vis
+if [ $4 ]; then 
+  SCHEMA_NAME=$4
+fi
 
 ### just includes a single database's objects (not users, etc)
-pg_dump --file $FILENAME --host $HOST --port "5434" --username "postgres" --verbose --format=c --blobs $DATABASE_NAME
+pg_dump --file $FILENAME --host $HOST --port "5432" --username $PG_USERNAME --verbose --format=c -d $DATABASE_NAME -n $SCHEMA_NAME
 
 ### includes all database and other objects
 # pg_dumpall -U postgres -h $HOST -p 5434 --clean --file $FILENAME
