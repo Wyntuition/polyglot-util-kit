2525e27d7d7cf9e66b96221899e75ea9609c4d71
Wyn Van Devanter <wyn.vandevanter@excella.com>
2019-11-15 16:29:42 -0500

    table sizes

--- postgres/activity-and-locks.sql
+++ postgres/activity-and-locks.sql
@@ -62,3 +62,14 @@ ALTER ROLE wbvandev WITH ENCRYPTED PASSWORD 'HelloWhere101!' VALID UNTIL 'infini
 select datname, pg_size_pretty(pg_database_size(datname))
 from pg_database
 order by pg_database_size(datname) desc;
+
+-- Biggest TABLE
+SELECT nspname || '.' || relname AS "relation",
+    pg_size_pretty(pg_total_relation_size(C.oid)) AS "total_size"
+  FROM pg_class C
+  LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
+  WHERE nspname NOT IN ('pg_catalog', 'information_schema')
+    AND C.relkind <> 'i'
+    AND nspname !~ '^pg_toast'
+  ORDER BY pg_total_relation_size(C.oid) DESC
+  LIMIT 20;
