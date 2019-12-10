d671a34caad72b099663e50cce354f247c0b7ad8
Wyn Van Devanter <wyn.vandevanter@excella.com>
2019-12-10 12:52:52 -0500

    basic version of cert info

--- ssl-cert-info-from-host.sh
+++ ssl-cert-info-from-host.sh
@@ -1,4 +1,8 @@
-$HOSTNAME=xyz.abc.org
-$PORT=443
+HOSTNAME=xyz.abc.com
+PORT=443
 
-echo | openssl s_client -showcerts -servername  -connect $HOSTNAME:$PORT 2>/dev/null | openssl x509 -inform pem -noout -text
+# Details
+echo | openssl s_client -showcerts -servername $HOSTNAME -connect $HOSTNAME:$PORT 2>/dev/null | openssl x509 -inform pem -noout -text
+
+# Basic
+openssl s_client -host $HOSTNAME -port $PORT -prexit -showcerts
